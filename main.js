4f49c9ff839d84f5fa3c17c574234a4b63ec62f4
Gabrielle Almeida <gabi.antunes.almeida@gmail.com>
2020-11-29 01:00:32 -0300

    criada as funções de ordenação

--- main.js
+++ main.js
@@ -1,68 +1,96 @@
-import { selectType, selectResistant, selectWeaknesses, search } from './data.js'
-import data from './data/pokemon/pokemon.js'
+import {
+  sortPokemonsByNameAscending,
+  sortPokemonsByNameDescending,
+  sortPokemonsByNumAscending,
+  sortPokemonsByNumDescending,
+  selectType,
+  selectResistant,
+  selectWeaknesses,
+  search,
+} from "./data.js";
+import data from "./data/pokemon/pokemon.js";
 
 //console.log(selectType, selectResistant, selectWeaknesses, search, data)
 
-const selectOrderly = document.querySelector('#select-orderly')
-const selectFilterType = document.querySelector('#select-filter-type')
-const selectFilterResistant = document.querySelector('#select-filter-resistant')
-const selectFilterWeaknesses = document.querySelector('#select-filter-weaknesses')
-const inputSearch = document.querySelector('#input-search')
+const selectOrderly = document.querySelector("#select-orderly");
+const selectFilterType = document.querySelector("#select-filter-type");
+const selectFilterResistant = document.querySelector(
+  "#select-filter-resistant"
+);
+const selectFilterWeaknesses = document.querySelector(
+  "#select-filter-weaknesses"
+);
+const inputSearch = document.querySelector("#input-search");
 
-displayCards(data.pokemon)
+displayCards(data.pokemon);
 
 function displayCards(pokemons) {
-    let printCards = document.querySelector('#print-cards')
-    printCards.innerHTML = ''
+  let printCards = document.querySelector("#print-cards");
+  printCards.innerHTML = "";
 
-    for (let pokemon of pokemons) {
-        const div = document.createElement("div")
+  for (let pokemon of pokemons) {
+    const div = document.createElement("div");
 
-        const image = document.createElement("img")
-        image.src = pokemon.img
-        div.appendChild(image)
+    const image = document.createElement("img");
+    image.src = pokemon.img;
+    div.appendChild(image);
 
-        const name = document.createElement("h2")
-        name.innerHTML = pokemon.name
-        div.appendChild(name)
+    const name = document.createElement("h2");
+    name.innerHTML = pokemon.name;
+    div.appendChild(name);
 
-        const number = document.createElement("p")
-        number.innerHTML = pokemon.num
-        div.appendChild(number)
+    const number = document.createElement("p");
+    number.innerHTML = pokemon.num;
+    div.appendChild(number);
 
-        const type = document.createElement("p")
-        type.innerHTML = pokemon.type
-        div.appendChild(type)
-//falta resistant e weaknesses
-        printCards.appendChild(div)
-    }
+    const type = document.createElement("p");
+    type.innerHTML = pokemon.type;
+    div.appendChild(type);
+    //falta resistant e weaknesses
+    printCards.appendChild(div);
+  }
 }
 
-selectFilterType.addEventListener('change', () => {
-    const filterType = selectFilterType.value
-    const pokemonType = selectType(filterType, data.pokemon)
-    displayCards(pokemonType)
-})
+selectOrderly.addEventListener("change", () => {
+  const orderly = selectOrderly.value;
+  if (orderly === "NameAscending") {
+    sortPokemonsByNameAscending(data.pokemon);
+    displayCards(data.pokemon);
+  } else if (orderly === "NameDescending") {
+    sortPokemonsByNameDescending(data.pokemon);
+    displayCards(data.pokemon);
+  } else if (orderly === "NumAscending") {
+    sortPokemonsByNumAscending(data.pokemon);
+    displayCards(data.pokemon);
+  } else if (orderly === "NumDescending") {
+    sortPokemonsByNumDescending(data.pokemon);
+    displayCards(data.pokemon);
+  }
+});
 
-selectFilterResistant.addEventListener('change', () => {
-    const filterResistant = selectFilterResistant.value
-    const pokemonResistant = selectResistant(filterResistant, data.pokemon)
-    displayCards(pokemonResistant)
-})
+selectFilterType.addEventListener("change", () => {
+  const filterType = selectFilterType.value;
+  const pokemonType = selectType(filterType, data.pokemon);
+  displayCards(pokemonType);
+});
 
-selectFilterWeaknesses.addEventListener('change', () => {
-    const filterWeaknesses = selectFilterWeaknesses.value
-    const pokemonWeaknesses = selectWeaknesses(filterWeaknesses, data.pokemon)
-    displayCards(pokemonWeaknesses)
-})
+selectFilterResistant.addEventListener("change", () => {
+  const filterResistant = selectFilterResistant.value;
+  const pokemonResistant = selectResistant(filterResistant, data.pokemon);
+  displayCards(pokemonResistant);
+});
+
+selectFilterWeaknesses.addEventListener("change", () => {
+  const filterWeaknesses = selectFilterWeaknesses.value;
+  const pokemonWeaknesses = selectWeaknesses(filterWeaknesses, data.pokemon);
+  displayCards(pokemonWeaknesses);
+});
 // Falta fazer essa parte!!!
-inputSearch.addEventListener('keyup', () => {
-    const searchPokemon = inputSearch.value
-    const Pokemon = search(searchPokemon, data.pokemon)
-    displayCards(Pokemon)
-})
-
-
+inputSearch.addEventListener("keyup", () => {
+  const searchPokemon = inputSearch.value;
+  const Pokemon = search(searchPokemon, data.pokemon);
+  displayCards(Pokemon);
+});
 
 //import { example } from './data.js';
 // import data from './data/lol/lol.js';
@@ -70,5 +98,3 @@ inputSearch.addEventListener('keyup', () => {
 // import data from './data/rickandmorty/rickandmorty.js';
 
 //console.log(example, data);
-
-
